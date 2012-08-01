3b233d69126498cd6ef9b9c2294884a2844cba0c
Anthony Grimes <i@raynes.me>
2012-07-31 22:48:01 -0500

    Update conch.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [conch "0.3.0"]
+                 [conch "0.3.1"]
                  [bultitude "0.1.7"]]
   :main bcs.core/-main)
