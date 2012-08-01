e9bb1380498fbeab7e913f42b0f1dcdf76086312
Anthony Grimes <i@raynes.me>
2012-08-01 16:36:54 -0500

    Move raw-time to useful.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [conch "0.3.1"]
-                 [bultitude "0.1.7"]]
+                 [bultitude "0.1.7"]
+                 [useful "0.8.4-alpha1"]]
   :main bcs.core/-main)
--- src/bcs/tools.clj
+++ src/bcs/tools.clj
@@ -1,5 +1,6 @@
 (ns bcs.tools
-  (:require [conch.core :as conch]))
+  (:require [conch.core :as conch]
+            [useful.state :refer [with-timing]]))
 
 (def ^:dynamic *timeout* 10000)
 
@@ -8,22 +9,13 @@
    and :code"
   :lang)
 
-(defmacro raw-time
-  "Same as clojure.core/time but returns a vector of a the result of
-   the code and the milliseconds rather than printing a string and runs
-   code in an implicit do."
-  [& body]
-  `(let [start# (System/nanoTime)
-         ret# ~(cons 'do body)]
-     [ret# (/ (double (- (System/nanoTime) start#)) 1000000.0)]))
-
 (defn sandboxed
   "Run the application in the sandbox with args."
   [app & args]
   (let [proc (apply conch/proc "unbuffer" app args)
         out (future (conch/stream-to-string proc :out))
         err (future (conch/stream-to-string proc :err ))]
-    (let [[exit-code ms] (raw-time (conch/exit-code proc *timeout*))]
+    (let [[exit-code ms] (with-timing (conch/exit-code proc *timeout*))]
       {:exit-code exit-code
        :out @out
        :err @err
